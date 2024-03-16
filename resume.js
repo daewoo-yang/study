09189651004de7367c06fc9d3733909d98a3df51
daewooyang <daewoo0109@gmail.com>
2024-03-16 21:10:32 +0900

    first commit

---
+++ resume.js
@@ -0,0 +1,17 @@
+function showProfileModal() {
+  // console.log(document.querySelector('.profileModal'));
+  document.querySelector('.profileModal').style.display = 'grid';
+}
+
+function hideProfileModal() {
+  document.querySelector('.profileModal').style.display = 'none';
+}
+
+//
+let ex1 = document.getElementById("modal").style.display = 'none'; //id를 찾아간다
+
+
+let ex1 = document.getElementById(""); //id를 찾아간다
+let ex1 = document.getElementById(""); //id를 찾아간다
+let ex1 = document.getElementById(""); //id를 찾아간다
+
